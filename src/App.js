5ebc9a5f545ced7aa0c0de7030b2d5a656aed52b
DavidRZuluaga <git@davidrzuluaga.com>
2018-11-10 00:12:47 -0500

    ej

--- src/App.js
+++ src/App.js
@@ -2,29 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-
-  constructor() {
-    super();
-    
-    this.state = {
-      seeSentence: false
-    }
-  }
+	constructor(){
+		super()
+		this.state = {
+			checked: false
+		}
+	}
   render() {
     return (
       <div className="wrapper">
-        <label><input type="checkbox" checked={this.state.seeSentence}  onClick = {this.checkSentence.bind(this)}/> Mostrar información importante</label>
-        {this.state.seeSentence ? <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> : ""}
+        <label><input type="checkbox" onChange={this.show.bind(this)} /> Mostrar información importante</label>
+        { this.state.checked ? <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> : ""}
       </div>
     );
   }
-  checkSentence(event){
-    this.setState({
-      seeSentence: event.target.checked,
-    })
-
+  show(){
+  	this.setState({
+			checked: !this.state.checked
+  	});
   }
-
 }
 
 export default App;
